7a5101a6ffe0fb756fefadb2eb9e3f0302925d5f
Siddico <www.mohammedahmedmohammedsiddiq@gmail.com>
2025-12-20 01:21:15 +0200

    task ui

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:build_ui/constants/app_font_styles.dart';
 import 'package:build_ui/constants/constant_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:svg_flutter/svg_flutter.dart';
 
@@ -91,50 +92,67 @@ class HomeView extends StatelessWidget {
 
           const SizedBox(height: 12),
           Container(
-            padding: EdgeInsets.all(14),
             height: 250,
-            width: 175,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(18),
-              border: Border.all(color: Color(0xffe2e2e2), width: 1),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SvgPicture.asset('Assets/Images/banana_image.svg', height: 80),
-                const SizedBox(height: 25),
-                Text(
-                  'Organic Bananas',
-                  style: AppFontStyles.fontSize16Weight400WithColorBlack,
-                ),
-                const SizedBox(height: 6),
-                Text(
-                  '7pcs',
-                  style: AppFontStyles.fontSize14Weight600WithGreyColor
-                      .copyWith(fontWeight: FontWeight.w400),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      r'$4.99',
-                      style: AppFontStyles.fontSize24Weight600WithColorBlack
-                          .copyWith(fontSize: 18),
+            child: ListView.builder(
+              itemCount: 10,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(left: 14.0),
+                  child: Container(
+                    padding: EdgeInsets.all(14),
+                    height: 250,
+                    width: 175,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                      border: Border.all(color: Color(0xffe2e2e2), width: 1),
                     ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SvgPicture.asset(
+                          'Assets/Images/banana_image.svg',
+                          height: 80,
+                        ),
+                        const SizedBox(height: 25),
+                        Text(
+                          'Organic Bananas',
+                          style:
+                              AppFontStyles.fontSize16Weight400WithColorBlack,
+                        ),
+                        const SizedBox(height: 6),
+                        Text(
+                          '7pcs',
+                          style: AppFontStyles.fontSize14Weight600WithGreyColor
+                              .copyWith(fontWeight: FontWeight.w400),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              r'$4.99',
+                              style: AppFontStyles
+                                  .fontSize24Weight600WithColorBlack
+                                  .copyWith(fontSize: 18),
+                            ),
 
-                    Container(
-                      height: 46,
-                      width: 46,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(17),
-                        color: Color(0xff53b175),
-                      ),
-                      child: Icon(Icons.add, color: Colors.white),
+                            Container(
+                              height: 46,
+                              width: 46,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(17),
+                                color: Color(0xff53b175),
+                              ),
+                              child: Icon(Icons.add, color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                );
+              },
             ),
           ),
         ],
